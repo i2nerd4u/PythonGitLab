9c1cd8be65ac0b333706621a755e33e332679d75
i2nerd4u <karl360@live.ie>
2025-10-03 00:09:37 +0100

    Add basic calculator function

--- GitBasicsLab.py
+++ GitBasicsLab.py
@@ -3,8 +3,5 @@ def greet(name):
     
 print(greet("Student"))
 print("This line was added directly on GitHub.")
-<<<<<<< HEAD
-print("This line was added locally.")
-=======
 print("Another line added on GitHub.")
->>>>>>> 11a4c9a0b492058beeb78ffff91fe3c51ee81789
+print("This line was added locally.")
